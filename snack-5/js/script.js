7ce06262721c70c17afe0d64b9546be8373043a6
Mauro Tomasoni <mauro.tom.vn@gmail.com>
2021-08-25 18:26:50 +0200

    add style

--- snack-5/js/script.js
+++ snack-5/js/script.js
@@ -1,12 +1,12 @@
 // Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array;
 var odd = [];
 
-for (var i = 0; i < 6; i++){
+for (var i = 0; i < 6; i++) {
   var number = parseInt(prompt('Insert a number'));
   if (number % 2 != 0) {
     odd.push(number);
   }
 }
 
-
+document.getElementById('odd').innerHTML += odd;
 console.log(`The odd numbers are ${odd}`);
